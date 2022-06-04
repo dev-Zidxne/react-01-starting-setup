955ab22a29c92d524a50564f886ff3906d1138c3
Zidane Innis <zidaneinnis@gmail.com>
2022-06-04 10:39:55 -0500

    Working on the ExpenseForm

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import logo from "./logo.svg";
-import "./App.css";
-import Expenses from "./components/Expenses/Expenses";
-
 import React from "react";
+
+import NewExpense from "./components/NewExpense/NewExpense";
+import Expenses from "./components/Expenses/Expenses";
 
 const App = () => {
   const expenses = [
@@ -36,7 +35,7 @@ const App = () => {
 
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense />
       <Expenses items={expenses} />
     </div>
   );
